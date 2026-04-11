5ec0396d6ca424b11fac4cc69db4a83a551c4ccd
Laela Rustiani <laelarustiani@email.com>
2026-04-12 03:42:48 +0700

    fix: redirect logs to stderr for vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,25 @@
 <?php
 
+if (!is_dir('/tmp/views')) {
+    mkdir('/tmp/views', 0755, true);
+}
+
+if (!is_dir('/tmp/storage/logs')) {
+    mkdir('/tmp/storage/logs', 0755, true);
+}
+
+if (!is_dir('/tmp/storage/framework/sessions')) {
+    mkdir('/tmp/storage/framework/sessions', 0755, true);
+}
+
+if (!is_dir('/tmp/storage/framework/cache')) {
+    mkdir('/tmp/storage/framework/cache', 0755, true);
+}
+
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp/views';
+$_ENV['LOG_CHANNEL'] = 'stderr';
+
+putenv('VIEW_COMPILED_PATH=/tmp/views');
+putenv('LOG_CHANNEL=stderr');
+
 require __DIR__ . '/../public/index.php';
